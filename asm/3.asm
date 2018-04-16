3e2231046dc2b31a33d4a1c6f3b329c80971912f
Derriick <derriick.ensiie@yahoo.com>
2018-04-16 22:26:05 +0200

    Correction saut conditionnel

--- asm/3.asm
+++ asm/3.asm
@@ -1,7 +1,9 @@
-	ldi r1, 42
-	subi r0, r1, 17
-	cmp r0, r1
-	cmp r1, r0, 1
-	add r2, r0, r1, 0
-	sub r1, r0, r0, 2
+	nop
+	in r0
+	out r0
+	in r1
+	out r1
+	out r0
+	addi r0, r1, 2
+	out r0
 end:	jmp end
--- asm/4.asm
+++ asm/4.asm
@@ -1,7 +1,11 @@
 	ldi r1, 42
 	subi r0, r1, 17
-	cmp r0, r1
-	cmp r1, r0, 1
-	add r2, r0, r1, 0
-	sub r1, r0, r0, 2
+	jeq r0, r1, j1
+	jlt r0, r1, j1
+	jle r0, r1, j1
+	cmpi r1, 42
+	add r2, r0, r1, 1
+	jeq r2, r1, end
+j1:	sub r1, r0, r0, 2
+	cmp r0, r0, 0
 end:	jmp end
